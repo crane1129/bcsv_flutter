d6a739fa6b9f7ca540dd79d55fef06d3d1bb65ca
Kevin Kim <kevin.kim@cyberark.com>
2022-01-10 13:59:43 -0800

    change code

--- lib/services/api_endpoint.dart
+++ lib/services/api_endpoint.dart
@@ -4,49 +4,46 @@ import 'dart:convert';
 import 'package:bcsv_flutter_project/data_models/endpoint_model.dart';
 import 'package:bcsv_flutter_project/screens/home_screen.dart';
 
+const String kEndpointAPI =
+    "https://script.google.com/macros/s/AKfycbwW_u3urSmxnQrIFsPxwVVzvbNnAtscBZGvxcRfYzJXuLQEWMNB/exec";
 
-const String kEndpointAPI = "https://script.google.com/macros/s/AKfycbwW_u3urSmxnQrIFsPxwVVzvbNnAtscBZGvxcRfYzJXuLQEWMNB/exec";
-
-class ApiEndpoint{
+class ApiEndpoint {
   static var apiMap = {};
 
   static void bindEndpoints(BuildContext context) async {
-
-    try{
+    try {
       String endpointList = await _getApiEndpoints();
       var jsonObj = jsonDecode(endpointList)['endpoints'] as List;
-      List<Endpoint> endpointObjs = jsonObj.map((tagJson) => Endpoint.fromJson(tagJson)).toList();
+      List<Endpoint> endpointObjs =
+          jsonObj.map((tagJson) => Endpoint.fromJson(tagJson)).toList();
 
-      for (Endpoint e in endpointObjs){
+      for (Endpoint e in endpointObjs) {
         apiMap[e.endpoint] = e.url;
       }
 
       print('Endpoint bind is complete');
-      Navigator.push(
-        context,
-        MaterialPageRoute(
-          builder: (context) {
-            return MyHomePage();
-          },
-        ),
-      );
-    }
-    catch(e){
+
+      //Navigate to Home screen and destroy the loading screen.
+      Navigator.pushAndRemoveUntil(context, MaterialPageRoute(
+        builder: (context) {
+          return MyHomePage();
+        },
+      ), (e) => false);
+    } catch (e) {
       print(e.toString());
     }
   }
 
-  static Future <String> _getApiEndpoints() async{
+  static Future<String> _getApiEndpoints() async {
+    http.Response response = await http.get(Uri.parse(kEndpointAPI));
 
-      http.Response response = await http.get(Uri.parse(kEndpointAPI));
+    if (response.statusCode == 200) {
+      String data = response.body;
+      return data;
+    } else {
+      print(response.statusCode);
+    }
 
-      if (response.statusCode == 200){
-        String data = response.body;
-        return data;
-      }else{
-        print(response.statusCode);
-      }
-
-      return "";
+    return "";
   }
 }
